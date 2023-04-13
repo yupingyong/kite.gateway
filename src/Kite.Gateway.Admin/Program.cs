670205a06243077ef1e5e778c0e2268fbf0853a2
yupingyong <yupingyong@qqcom>
2023-04-13 23:15:05 +0800

    123456

--- src/Kite.Gateway.Admin/Program.cs
+++ src/Kite.Gateway.Admin/Program.cs
@@ -1,34 +1,23 @@
-
-using Kite.Gateway.Admin;
-using Kite.Gateway.Admin.Core;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Http.Features;
-using Serilog;
-using Serilog.Events;
-
 var builder = WebApplication.CreateBuilder(args);
 
-//日志配置
-builder.Logging.ClearProviders();
-Log.Logger = new LoggerConfiguration()
-          .Enrich.FromLogContext()
-          .WriteTo.Console()// 日志输出到控制台
-          .WriteTo.File($"data/logs/log-.txt", restrictedToMinimumLevel: LogEventLevel.Warning, rollingInterval: RollingInterval.Day)
-          .MinimumLevel.Information()
-          .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-          .CreateLogger();
-builder.Host
-     .UseSerilog(dispose:true)
-     .UseAutofac();
-//配置文件
-builder.Services.ReplaceConfiguration(builder.Configuration);//修正配置错误
-//基础组件注入
-builder.Services.AddApplication<GatewayAdminModule>();
-builder.Services.AddHttpClient();
-builder.Services.AddSingleton<AuthorizationServerStorage>();
+// Add services to the container.
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
-await app.InitializeApplicationAsync();
-await app.RunAsync();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
