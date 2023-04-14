50d3f0b1677891de0c89c4d908da5d3cf0297356
yupingyong <yupingyong@qq.com>
2023-04-14 14:00:43 +0800

    1111111111

--- src/Kite.Gateway.Admin/Controllers/HomeController.cs
+++ src/Kite.Gateway.Admin/Controllers/HomeController.cs
@@ -1,32 +1,21 @@
-﻿using Kite.Gateway.Admin.Models;
-using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Kite.Gateway.Admin.Controllers
 {
+    [Area("Admin")]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        public IActionResult Main()
         {
             return View();
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Login()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
     }
 }
