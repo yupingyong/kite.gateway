a3417c8e6e2c813033d61fac2b234f65909cfe70
yupingyong <yupingyong@qqcom>
2023-05-07 23:42:24 +0800

    更新状态

---
+++ src/Kite.Gateway.Admin/Extensions/AbpCoreExceptionFilter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using Serilog;
+using Microsoft.AspNetCore.Mvc;
+using Kite.Gateway.Application;
+
+namespace Kite.Gateway.Admin.Extensions
+{
+    public class AbpCoreExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            Log.Error(context.Exception,context.Exception.Message);
+
+            context.Result = new JsonResult(new KiteResult()
+            {
+                Code = 501,
+                Message = context.Exception.Message
+            });
+            context.ExceptionHandled = true;
+        }
+    }
+}
--- src/Kite.Gateway.Admin/GatewayAdminModule.cs
+++ src/Kite.Gateway.Admin/GatewayAdminModule.cs
@@ -20,6 +20,13 @@ using Kite.Gateway.Application.Contracts;
 using Kite.Gateway.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using Volo.Abp.Swashbuckle;
+using Microsoft.OpenApi.Models;
+using Volo.Abp.AspNetCore.Mvc;
+using Kite.Gateway.Admin.Extensions;
+using Volo.Abp.AspNetCore.Mvc.ExceptionHandling;
+using Volo.Abp.AspNetCore.Mvc.Validation;
 
 namespace Kite.Gateway.Admin
 {
@@ -27,39 +34,119 @@ namespace Kite.Gateway.Admin
         typeof(AbpAspNetCoreModule),
          typeof(AbpAutofacModule),
          typeof(ApplicationModule),
-         typeof(EntityFrameworkCoreModule)
+         typeof(EntityFrameworkCoreModule),
+         typeof(AbpSwashbuckleModule)
      )]
     public class GatewayAdminModule:AbpModule
     {
         #region 中间件注入
+        private ServiceConfigurationContext _context;
         public override void ConfigureServices(ServiceConfigurationContext context)
         {
+            _context = context;
             context.Services.AddMemoryCache();
             context.Services.AddHttpClient();
-            ConfigureMvc(context);
+            ConfigureMvc();
+            ConfigureSwaggerGen();
+            ConfigureConventionalControllers();
         }
-        private void ConfigureMvc(ServiceConfigurationContext context)
+        private void ConfigureMvc()
         {
-            context.Services.AddControllersWithViews();
-            context.Services.AddSession();
+            _context.Services.AddControllersWithViews(options =>
+            {
+                // 移除 AbpValidationActionFilter
+                var filterMetadata = options.Filters.FirstOrDefault(x => x is ServiceFilterAttribute attribute && attribute.ServiceType.Equals(typeof(AbpValidationActionFilter)));
+                if (filterMetadata != null)
+                {
+                    options.Filters.Remove(filterMetadata);
+                }
+                //移除全局异常过滤器
+                var errIndex = options.Filters.ToList().FindIndex(filter => filter is ServiceFilterAttribute attr && attr.ServiceType.Equals(typeof(AbpExceptionFilter)));
+                if (errIndex > -1)
+                {
+                    options.Filters.RemoveAt(errIndex);
+                }
+                //
+                options.Filters.Add(typeof(AbpCoreExceptionFilter));
+                options.Filters.Add<ValidateFilter>(-1);
+            });
+            Configure<AbpJsonOptions>(options => options.OutputDateTimeFormat = "yyyy-MM-dd HH:mm:ss");
+            _context.Services.AddSession();
+            //如果是Blazor配置专用
             Configure<HubOptions>(options =>
             {
                 options.DisableImplicitFromServicesParameters = true;
             });
         }
-       
-        
+        /// <summary>
+        /// 配置Swagger文档
+        /// </summary>
+        /// <param name="context"></param>
+        private void ConfigureSwaggerGen()
+        {
+            //在此处注入依赖项
+            _context.Services.AddAbpSwaggerGen(
+                options =>
+                {
+                    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Kite.Gateway.Api", Version = "v1" });
+                    var appServicesXmlPath = Path.Combine(AppContext.BaseDirectory, $"Kite.Gateway.Application.xml");
+                    if (File.Exists(appServicesXmlPath))
+                    {
+                        options.IncludeXmlComments(appServicesXmlPath, true);
+                    }
+                    var appContractsServicesXmlPath = Path.Combine(AppContext.BaseDirectory, $"Kite.Gateway.Application.Contracts.xml");
+                    if (File.Exists(appContractsServicesXmlPath))
+                    {
+                        options.IncludeXmlComments(appContractsServicesXmlPath, true);
+                    }
+                    options.DocInclusionPredicate((docName, description) => true);
+                    options.CustomSchemaIds(type => type.FullName);
+                    options.DocumentFilter<HiddenApiFilter>();
+                    options.DocumentFilter<SwaggerEnumFilter>();
+                });
+        }
+        /// <summary>
+        /// 自动API控制器
+        /// </summary>
+        private void ConfigureConventionalControllers()
+        {
+            Configure<AbpAspNetCoreMvcOptions>(options =>
+            {
+                options.ConventionalControllers.Create(typeof(ApplicationModule).Assembly, opts =>
+                {
+                    opts.RootPath = "kite";
+                });
+            });
+
+        }
+
         #endregion
         public override void OnApplicationInitialization(ApplicationInitializationContext context)
         {
             var app = context.GetApplicationBuilder();
             var env = context.GetEnvironment();
 
-            app.UseStaticFiles();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger(options =>
+                {
+                    options.RouteTemplate = "{documentName}/swagger.json";
+                });
+                app.UseSwaggerUI(options =>
+                {
+                    options.RoutePrefix = "";
+                    options.SwaggerEndpoint("v1/swagger.json", "Kite.Gateway.Api");
+                });
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
             app.UseSession();
+            app.UseStaticFiles();
             app.UseRouting();
-            
-            app.UseExceptionHandler("/Error");
+            app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
